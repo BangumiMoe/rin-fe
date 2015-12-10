c528c3f7477836e4e7ef384e73106fa72fd5ade8
IntPtr <lxy.lixiaoyan@gmail.com>
2015-12-11 01:37:34 +0800

    update

--- src/api/torrent.js
+++ src/api/torrent.js
@@ -3,5 +3,5 @@ import * as http from "../helper/http";
 
 export default {
   item: ({id}) => http.post("/api/torrent/fetch", {_id: id}),
-  list: ({page}) => http.get(uri.escape`/api/torrent/page/${page}`)
+  list: ({page}) => http.get(uri.escape`/api/v2/torrent/page/${page}`)
 };
--- src/view/components/torrent-list.js
+++ src/view/components/torrent-list.js
@@ -1,9 +1,13 @@
 import React from "react";
+import {connect} from "react-redux";
 import {Link} from "react-router";
 
 import Loader from "./loader";
 
 @Loader.wrap
+@connect(state => ({
+  language: state.language
+}))
 export default class TorrentList extends React.Component {
   render() {
     const data = this.props.data;
@@ -16,8 +20,12 @@ export default class TorrentList extends React.Component {
                 <h3 className="ui-torrentList__title">{item.title}</h3>
                 <div className="ui-torrentList__info">{item.publish_time}</div>
               </div>
-              <div className="ui-torrentList__marker">{item.downloads}</div>
-              <div className="ui-torrentList__action"></div>
+              <div className="ui-torrentList__category">{item.category_tag.locale[this.props.language]}</div>
+              <div className="ui-torrentList__action">
+                <button type="button" className="ui-button ui-button--iconOnly">
+                  <span className="ui-icon">file_download</span>
+                </button>
+              </div>
             </Link>
           </li>
         ))}
