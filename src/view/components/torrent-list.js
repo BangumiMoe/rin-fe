955c806332e0a6ee41c23d923d91a6e841a12a83
IntPtr <lxy.lixiaoyan@gmail.com>
2015-12-30 16:08:35 +0800

    update

--- src/view/components/torrent-list.js
+++ src/view/components/torrent-list.js
@@ -2,6 +2,8 @@ import React from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router";
 
+import {FormattedRelative} from "react-intl";
+
 import Button from "./button";
 import Loader from "./loader";
 import Pagination from "./pagination";
@@ -23,7 +25,15 @@ export default class TorrentList extends React.Component {
               <Link to={router.build("torrent", {id: item._id})} className="waves-effect">
                 <div className="ui-torrentList__main">
                   <h3 className="ui-torrentList__title">{item.title}</h3>
-                  <div className="ui-torrentList__info">{item.publish_time}</div>
+                  <div className="ui-torrentList__info">
+                    <div className="ui-torrentList__info__time">
+                      <FormattedRelative value={new Date(item.publish_time)} />
+                    </div>
+                    <div className="ui-torrentList__info__user">{item.uploader.username}</div>
+                    {item.team ? (
+                      <div className="ui-torrentList__info__team">{item.team.name}</div>
+                    ) : null}
+                  </div>
                 </div>
                 <div className="ui-torrentList__category">{item.category_tag.locale[this.props.language]}</div>
                 <div className="ui-torrentList__action">
